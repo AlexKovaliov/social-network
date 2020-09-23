f644a0132e1d562069d2193dadb287a3fe28bb7b
AlexKovaliov <lawsansana@gmail.com>
2020-09-23 09:31:27 +0300

    Side Effect

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -12,15 +12,17 @@ type UsersComponentsPropsType = {
 }
 
 export let Users = (props: UsersComponentsPropsType) => {
-    if (props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
                 props.setUsers(response.data.items)
-            }
-        )
+            })
+        }
     }
 
     return (
         <div>
+            <button onClick={getUsers}>Get Users</button>
             {
                 props.users.map(u => <div key={u.id}>
                     <span>
