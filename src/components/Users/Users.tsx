d98748192cae88b8f39d3fd78fb3fd54fecd0a8e
AlexKovaliov <lawsansana@gmail.com>
2020-09-25 15:01:01 +0300

    Users class component, extends React.Component

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -11,6 +11,7 @@ type UsersComponentsPropsType = {
     setUsers: (users: Array<UsersInformationType>) => void
 }
 
+/*
 export let Users = (props: UsersComponentsPropsType) => {
     let getUsers = () => {
         if (props.users.length === 0) {
@@ -59,4 +60,55 @@ export let Users = (props: UsersComponentsPropsType) => {
             }
         </div>
     )
+}*/
+
+export class Users extends React.Component<UsersComponentsPropsType> {
+
+    constructor(props: any) {
+        super(props);
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            this.props.setUsers(response.data.items)
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                {this.props.users.map(u => <div key={u.id}>
+                    <span>
+                        <div>
+                            <img src={u.photos.small !== null ? u.photos.small : userPhoto}
+                                 className={styles.userPhoto}/>
+                        </div>
+
+                        <div>
+                            {u.followed ?
+                                <button onClick={() => {
+                                    this.props.unfollow(u.id)
+                                }}>Unsubscribe
+                                </button>
+                                : <button onClick={() => {
+                                    this.props.follow(u.id)
+                                }}>Subscribe</button>}
+                        </div>
+                    </span>
+
+                        <span>
+                        <span>
+                            <div>{u.name}</div>
+                            <div>{u.status}</div>
+                        </span>
+
+                        <span>
+                            <div>{u.location.country}</div>
+                            <div>{u.location.city}</div>
+                        </span>
+                    </span>
+                    </div>
+                )
+                }
+            </div>
+        )
+    }
 }
+
